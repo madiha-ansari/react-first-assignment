a19bf8dbc281f041de742365726a374670c58de3
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-05 17:34:17 +0500

    screen sized are attached in the coading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <p className='text-center text-blue-900 font-semibold text-4xl underline border border-r-8 border-l-8 border-black sm:font-thin bg-pink-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, repellat totam delectus, voluptate quis magnam aut ea facilis, voluptas a ex similique pariatur odit aliquid doloremque perspiciatis quidem aperiam eaque.</p>
+      <p className='text-center text-blue-900 font-semibold text-4xl underline border border-r-8 border-l-8 border-black sm:font-thin bg-pink-900 md:bg-gray-700 italic lg:bg-blue-400 xl:bg-yellow-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, repellat totam delectus, voluptate quis magnam aut ea facilis, voluptas a ex similique pariatur odit aliquid doloremque perspiciatis quidem aperiam eaque.</p>
     </div>
   )
 }
