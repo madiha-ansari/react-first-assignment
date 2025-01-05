8a2809b3e639941353a3afc46f7aae13d21aaa9c
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-05 17:25:22 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-center text-blue-900 font-semibold text-4xl underline border border-r-8 border-l-8 border-black sm:font-thin bg-pink-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, repellat totam delectus, voluptate quis magnam aut ea facilis, voluptas a ex similique pariatur odit aliquid doloremque perspiciatis quidem aperiam eaque.</p>
+    </div>
+  )
+}
+
+export default App
